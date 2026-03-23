c89d1ad8818ca62a695eb189c5e922fa73221003
gabby <hannahfrench1709@hotmail.com>
2026-03-23 20:15:04 +0000

    style: change font style and add styling to SearchBar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ubuntu = Ubuntu({
+  weight: ["300", "500", "700"],
+  style: ["normal", "italic"],
   subsets: ["latin"],
 });
 
@@ -18,10 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${ubuntu.className}`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,14 +16,23 @@ export default function SearchBar({ onSearch }) {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex gap-2 justify-center align-items"
+      >
         <input
           type="text"
           value={postcode}
           onChange={handleChange}
           placeholder="Enter postcode"
+          className="border-2 rounded-lg p-0.5 text-center border-(--border)"
         />
-        <button type="submit">Search</button>
+        <button
+          type="submit"
+          className="cursor-pointer px-2 border-2 rounded-lg p-0.5 border-(--border)"
+        >
+          Search
+        </button>
       </form>
     </div>
   );
