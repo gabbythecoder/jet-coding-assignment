fb4d4e209562a0a235bd088fcdb75f68626c79c7
gabby <hannahfrench1709@hotmail.com>
2026-03-25 20:40:05 +0000

    style: apply initial styling to RestaurantList

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,22 +1,57 @@
+import Image from "next/image";
+import cuisineTypeIcon from "@/../public/cuisine-type-icon.png";
+import ratingStarIcon from "@/../public/rating-star-icon.png";
+import pinLocationIcon from "@/../public/pin-location-icon.png";
+
 export default function RestaurantList({ restaurants }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 m-2.5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-2 m-10">
       {restaurants.slice(0, 10).map((restaurant) => (
-        <div key={restaurant.id} className="border-2 p-4 rounded-xl">
-          <h3>{restaurant.name}</h3>
+        <div
+          key={restaurant.id}
+          className="border-2 p-4 rounded-xl aspect-square overflow-hidden bg-cover"
+          style={{ backgroundImage: `url('/jet-logo.png')` }}
+        >
+          <h3 className="font-bold text-lg mb-1">{restaurant.name}</h3>
 
-          <p>
-            {restaurant.cuisines?.slice(0, 2).map((cuisine) => cuisine.name).join(", ") ||
-              "N/A"}
-          </p>
+          <div className="flex items-center gap-1">
+            <Image
+              src={cuisineTypeIcon}
+              alt="Cuisine Type Icon"
+              width={18}
+              height={18}
+            />
+            <span>
+              {restaurant.cuisines
+                ?.slice(0, 2)
+                .map((cuisine) => cuisine.name)
+                .join(", ") || "N/A"}
+            </span>
+          </div>
 
-          <p>Rating: {restaurant.rating?.starRating || "N/A"}</p>
+          <div className="flex items-center gap-1">
+            <Image
+              src={ratingStarIcon}
+              alt="Rating Star Icon"
+              width={18}
+              height={18}
+            />
+            <span>{restaurant.rating?.starRating || "N/A"}</span>
+          </div>
 
-          <p>
-            {restaurant.address
-              ? `${restaurant.address.firstLine}, ${restaurant.address.city}, ${restaurant.address.postalCode}`
-              : "N/A"}
-          </p>
+          <div className="flex items-start gap-1">
+            <Image
+              src={pinLocationIcon}
+              alt="Pin Location Icon"
+              width={18}
+              height={18}
+            />
+            <span className="wrap-break-word">
+              {restaurant.address
+                ? `${restaurant.address.firstLine}, ${restaurant.address.city}, ${restaurant.address.postalCode}`
+                : "N/A"}
+            </span>
+          </div>
         </div>
       ))}
     </div>
